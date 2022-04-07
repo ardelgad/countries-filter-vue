63afc45b58281da95373b9f6cb58a5a198bf4fdd
ardelgad <adelgadoporras@gmail.com>
2022-04-07 13:50:23 +0200

    clase

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
 // Suerte :)
+
+Vue.createApp({
+    data() {
+        return {
+            countries: [],
+            countryFilter: '',
+            regionFilter:''
+        }
+    },
+
+    created() {
+        this.fetchData();
+    },
+
+    computed: {
+        getFilteredCountries() {
+
+            let countries = this.countries;
+
+            if (this.countryFilter) {
+                // TODO: return this.countries.filter(...)
+                countries = this.countries.filter(country => country.name.includes(this.countryFilter));
+
+                return search;
+            }
+
+            if (this.regionFilter && this.regionFilter != 'all') {
+                console.log('filter region:' + this.regionFilter)
+                const reg = new RegExp(`${this.regionFilter}`, 'i')
+                countries = countries.filter(country => country.region.match(reg))
+            }
+            return this.countries;
+        }
+    },
+
+    methods: {
+        async fetchData() {
+            const response = await fetch("https://restcountries.com/v2/all");
+            this.countries = await response.json();
+        },        
+    },
+}).mount("#app")
